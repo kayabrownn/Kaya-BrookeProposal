b4f24df2716c6aa7233216ae3ba45c046f43dfa9
kayabrownn <tup37862@temple.edu>
2025-03-25 14:49:17 -0400

    Add files via upload
    pre proposal as document

---
+++ preproposal.md.py
@@ -0,0 +1,17 @@
+Python 3.13.1 (v3.13.1:06714517797, Dec  3 2024, 14:00:22) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+... 
+... # Preproposal
+... 
+... ## What idea(s) do you have for your final project?
+... obstacle course via roblox studio (scripting)
+... race track where user can drive car and avoid driving off track
+... user selection of car end destination must be reached
+... multiple levels?
+... save user progress?
+... 
+... ##ou plan to collaborate with one or two classmates, what are their names?
+... Brooke Deangelis // Kaya Brown
+... 
+... ##ou have any questions of your own?
